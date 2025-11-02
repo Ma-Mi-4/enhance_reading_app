877251c02369f835ef21d826b460531462c0c524
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-02 15:05:58 +0900

    復習問題出題機能の完了 Close #12

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,31 +3,19 @@ class QuizzesController < ApplicationController
   before_action :load_quiz_data, only: [:show, :explanation] 
 
   def show
-    level = params[:level] || '600'
-    id = params[:id] || '001'
-    file_path = Rails.root.join("data/quiz/level#{level}/quiz_level#{level}_#{id}.json")
-
-    if File.exist?(file_path)
-      file = File.read(file_path)
-      @quiz_data = JSON.parse(file)
-    else
-      @quiz_data = {
-        "title" => "復習問題データが見つかりません",
-        "questions" => []
-      }
-    end
-  rescue JSON::ParserError
-    @quiz_data = {
-      "title" => "JSON読み込みエラー",
-      "questions" => []
-    }
   end
+
   def explanation
     # 復習解説用
     # @quiz_data["questions"][i]["explanation"] をビューで使える
   end
 
   private
+
+  def set_quiz_params
+    @level = params[:level] || '600'
+    @id = params[:id] || '001'
+  end
 
   def load_quiz_data
     level = params[:level] || '600'
