190a720fc96aea6d05cbae9d63a085dbb9ca35a2
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-24 19:49:03 +0900

    Googleログインの修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('CORS_ORIGINS', 'http://localhost:3002')
+    origins 'https://enhance-reading-app-morning-sound-6129.fly.dev', 'http://localhost:3002'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
