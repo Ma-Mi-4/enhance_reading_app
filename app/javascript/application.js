641aac96e690947aa11abe8e924f0d5181f955e3
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-26 19:44:08 +0900

    Fix importmap setup and Chart.js ES module registration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,14 @@
 console.log("application.js loaded");
-import "@hotwired/turbo-rails"
-import Alpine from "alpinejs"
-import "./timer" 
-import "./study_chart"
 
-window.Alpine = Alpine
-Alpine.start()
+import "@hotwired/turbo-rails";
+import Alpine from "alpinejs";
+import "controllers";
+import "./timer";
+import "./study_chart";
+import { Chart } from "chart.js";
+import annotationPlugin from "chartjs-plugin-annotation";
+
+Chart.register(annotationPlugin);
+
+window.Alpine = Alpine;
+Alpine.start();
