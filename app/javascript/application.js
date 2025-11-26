87a9eb135c7d83e37f3ec116a639890af686682f
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-26 21:56:37 +0900

    Fix chartjs annotation import for importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,9 +8,11 @@ import "timer";
 import "study_chart";
 
 import { Chart } from "chart.js";
-import annotationPlugin from "chartjs-plugin-annotation";
+import "chartjs-plugin-annotation";
 
-Chart.register(annotationPlugin);
+if (window['chartjs-plugin-annotation']) {
+  Chart.register(window['chartjs-plugin-annotation']);
+}
 
 window.Alpine = Alpine;
 Alpine.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -12,4 +12,4 @@ pin "study_chart", to: "study_chart.js"
 pin "@rails/ujs", to: "vendor/@rails--ujs.js"
 
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.esm.js"
-pin "chartjs-plugin-annotation", to: "https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation@4.2.1/dist/chartjs-plugin-annotation.esm.js"
+pin "chartjs-plugin-annotation", to: "https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation@4.2.1/dist/chartjs-plugin-annotation.umd.js"
