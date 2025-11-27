a4443b445943b9d5f5b272a0651866183fe4cace
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-27 17:10:01 +0900

    JSファイル読み込みエラーの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,11 @@
 console.log("application.js loaded");
 
 import "@hotwired/turbo-rails";
-import Alpine from "alpinejs";
 import "controllers";
+
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+Alpine.start();
 
 import "timer";
 import "study_chart";
@@ -11,6 +14,3 @@ import { Chart } from "chart.js";
 import annotationPlugin from "chartjs-plugin-annotation";
 
 Chart.register(annotationPlugin);
-
-window.Alpine = Alpine;
-Alpine.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,15 +1,15 @@
-pin "application", to: "application.js"
+pin "application"
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "alpinejs", to: "https://unpkg.com/alpinejs@3.x.x/dist/module.esm.js"
 
-pin "timer",       to: "timer.js"
+pin "timer", to: "timer.js"
 pin "study_chart", to: "study_chart.js"
-
-pin "@rails/ujs", to: "vendor/@rails--ujs.js"
 
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.esm.js"
 pin "chartjs-plugin-annotation",
