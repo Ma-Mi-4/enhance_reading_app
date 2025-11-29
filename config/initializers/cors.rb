e823633f33c90143b0130590900a94f6220cc789
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-29 22:20:12 +0900

    Fix session cookie domain for Next.js integration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://frontend-autumn-morning-199.fly.dev'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,5 @@
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store,
-    key: '_enhance_reading_app_session',
-    same_site: :none,
-    secure: true
-else
-  Rails.application.config.session_store :cookie_store,
-    key: '_enhance_reading_app_session'
-end
+Rails.application.config.session_store :cookie_store,
+  key: '_enhance_reading_app_session',
+  domain: '.fly.dev',
+  same_site: :none,
+  secure: true
