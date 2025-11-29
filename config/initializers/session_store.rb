3837549521ce5cf029881a1df75504496096b971
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-29 16:14:40 +0900

    Fix session store for prod/dev

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,9 @@
-Rails.application.config.session_store :cookie_store,
-  key: '_enhance_reading_app_session',
-  same_site: :none,
-  secure: true
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store,
+    key: '_enhance_reading_app_session',
+    same_site: :none,
+    secure: true
+else
+  Rails.application.config.session_store :cookie_store,
+    key: '_enhance_reading_app_session'
+end
