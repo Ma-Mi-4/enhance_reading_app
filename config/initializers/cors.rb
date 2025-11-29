ac23896e06e44f45f073a459552f5df79c189cd2
Ma-Mi-4 <marcie.misa1004@gmail.com>
2025-11-29 22:41:37 +0900

    Fix CORS for Fly deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://frontend-autumn-morning-199.fly.dev'
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
